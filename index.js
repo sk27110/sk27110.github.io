999cf52011dbafb47f7d6475c2ec3f44728e8da1
sk27110 <sashakoch27@gmail.com>
2024-05-01 19:29:17 +0300

    uytrew

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ function init() {
 		zoom: 17
 	});
 
-	let placemark = new ymaps.Placemark(center, {}, {
+	let placemark = new ymaps.Placemark(center, {
+		balloonContentHeader: 'Хедер балуна',
+		balloonContentBody: 'Боди балуна',
+		balloonContentFooter: 'Подвал',
+	}, {
+		iconLayout: 'default#image',
+		iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
+		iconImageSize: [40, 40],
+		iconImageOffset: [-19, -44]
+	});
+
+	let placemark1 = new ymaps.Placemark(center, {
+		balloonContent: `
+
+			<div class="balloon">
+				<div class="balloon__address">г. Париж</div>
+				<div class="balloon__contacts">
+					<a href="tel:+7999999999">+7999999999</a>
+				</div>
+			</div>
+
+		`
+	}, {
 		iconLayout: 'default#image',
 		iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
 		iconImageSize: [40, 40],
@@ -22,7 +44,13 @@ function init() {
   map.controls.remove('rulerControl'); // удаляем контрол правил
   // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 
-	map.geoObjects.add(placemark);
+	// map.geoObjects.add(placemark);
+	map.geoObjects.add(placemark1);
+
+	placemark1.balloon.open();
 }
 
 ymaps.ready(init);
+
+
+//<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=7240ea03-95e6-44f3-a9c4-e4b336df23ec">
