a42797d90a8a3197a9082b803b666ea11a9434b1
sk27110 <sashakoch27@gmail.com>
2024-05-01 19:21:33 +0300

    test

--- index.js
+++ index.js
@@ -1,68 +1,28 @@
-ymaps.ready(function () {
+let center = [48.8866527839977,2.34310679732974];
 
-    let myMap = new ymaps.Map('map-test', {
-      center: [59.91795236804815, 30.304908500000003],
-      zoom: 15,
-      controls: ['routePanelControl']
-    });
-  
-    let control = myMap.controls.get('routePanelControl');
-    let city = 'Санкт-Петербург';
-  
-    // let location = ymaps.geolocation.get();
-  
-    // location.then(function(res) {
-    // 	let locationText = res.geoObjects.get(0).properties.get('text');
-    // 	console.log(locationText)
-    // });
-  
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-  
-    function success(pos) {
-      const crd = pos.coords;
-  
-      console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude: ${crd.longitude}`);
-  
-  
-      let reverseGeocoder = ymaps.geocode([crd.latitude, crd.longitude]);
-      let locationText = null;
-      reverseGeocoder.then(function (res) {
-        locationText = res.geoObjects.get(0).properties.get('text')
-        console.log(locationText)
-  
-        control.routePanel.state.set({
-          type: 'masstransit',
-          fromEnabled: false,
-          from: locationText,
-          toEnabled: true,
-          to: `${city}, Невский проспект 146`,
-        });
-      });
-  
-      console.log(locationText)
-  
-      
-  
-      control.routePanel.options.set({
-        types: {
-          masstransit: true,
-          pedestrian: true,
-          taxi: true
-        }
-      })
-    }
-  
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-  
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  
-  
-  
-  });
+function init() {
+	let map = new ymaps.Map('map-test', {
+		center: center,
+		zoom: 17
+	});
+
+	let placemark = new ymaps.Placemark(center, {}, {
+		iconLayout: 'default#image',
+		iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
+		iconImageSize: [40, 40],
+		iconImageOffset: [-19, -44]
+	});
+
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+	map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
